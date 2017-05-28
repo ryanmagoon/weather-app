34f7b67db648d5894aa6804e81fc58b2be851d0d
Ryan Magoon <mr.magoon5@gmail.com>
2017-05-28 15:16:54 -0400

    implement weather list

--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -1,4 +1,9 @@
+import { FETCH_WEATHER } from '../actions/index'
+
 export default function(state = [], action) {
-  console.log('Action Recieved', action)
+  switch(action.type) {
+  case FETCH_WEATHER:
+    return state.concat([action.payload.data])
+  }
   return state
 }
